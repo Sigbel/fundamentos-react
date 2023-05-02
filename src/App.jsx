535052993aaac29f3d8074095079385340438d5c
Henrique <hb.belgamo@gmail.com>
2023-05-02 15:11:04 -0300

    Explicação - Contador

--- src/App.jsx
+++ src/App.jsx
@@ -15,33 +15,44 @@ import UsuarioInfo from './components/condicional/UsuarioInfo';
 import DiretaPai from './components/comunicacao/DiretaPai';
 import IndiretaPai from './components/comunicacao/IndiretaPai';
 import Input from './components/formulario/Input';
+import Contador from './components/contador/Contador';
 
 export default _ => (
     <div className='App'>
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+            <Card titulo="#12 - Contador" color="#424242">
+                <Contador numeroInicial={10}></Contador>
+            </Card>
+
             <Card titulo="#11 - Componente Controlado" color="#E45F56">
                 <Input></Input>
             </Card>
+            
             <Card titulo="#10 - Comunicação Indireta" color="#8BAD39">
                 <IndiretaPai></IndiretaPai>
             </Card>
+
             <Card titulo="#09 - Comunicação Direta" color="#59323C">
                 <DiretaPai></DiretaPai>
             </Card>
+
             <Card titulo="#08 - Rendereização Condicional" color="#982395">
                 <ParOuImpar numero={10}></ParOuImpar>
                 <UsuarioInfo usuario={{nome: 'Fernando'}}></UsuarioInfo>
                 <UsuarioInfo usuario={{email: 'fer@gmail.com'}}></UsuarioInfo>
                 <UsuarioInfo usuario={{}}></UsuarioInfo>
             </Card>
+
             <Card titulo="#07 - Repetição (Desafio)" color="#3A9AD9">
                 <TabelaProdutos></TabelaProdutos>
             </Card>
+
             <Card titulo="#06 - Repetição" color="#FF4C65">
                 <ListaAlunos></ListaAlunos>
             </Card>
+
             <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
                 <Familia sobrenome="Ferreira">
                     <FamiliaMembro nome="Pedro"/>
@@ -49,15 +60,18 @@ export default _ => (
                     <FamiliaMembro nome="Gustavo" sobrenome="Silva"/>
                 </Familia>
             </Card>
+
             <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
                 <Aleatorio
                     min={1}
                     max={6985}
                 />
             </Card>
+
             <Card titulo="#03 - Fragmento" color="#E94C6F">
                 <Fragmento />
             </Card>
+
             <Card titulo="#02 - Com Parâmetro" color="#E8B71A">
                 <ComParameter
                     titulo="Situação do Aluno"
@@ -65,6 +79,7 @@ export default _ => (
                     nota={8.3}
                 />
             </Card>
+
             <Card titulo="#01 - Primeiro Componente" color="#588C73">
                 <First></First>
             </Card>
