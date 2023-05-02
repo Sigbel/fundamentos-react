d9c7eacad2da774eb49dbe55a2365330aedb6896
Henrique <hb.belgamo@gmail.com>
2023-05-02 14:04:33 -0300

    Explicação - Comunicação Direta e Indireta

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,20 @@ import ListaAlunos from './components/repeticao/ListaAlunos';
 import TabelaProdutos from './components/repeticao/TabelaProdutos';
 import ParOuImpar from './components/condicional/ParOuImpar';
 import UsuarioInfo from './components/condicional/UsuarioInfo';
+import DiretaPai from './components/comunicacao/DiretaPai';
+import IndiretaPai from './components/comunicacao/IndiretaPai';
 
 export default _ => (
     <div className='App'>
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+            <Card titulo="#10 - Comunicação Indireta" color="#8BAD39">
+                <IndiretaPai></IndiretaPai>
+            </Card>
+            <Card titulo="#09 - Comunicação Direta" color="#59323C">
+                <DiretaPai></DiretaPai>
+            </Card>
             <Card titulo="#08 - Rendereização Condicional" color="#982395">
                 <ParOuImpar numero={10}></ParOuImpar>
                 <UsuarioInfo usuario={{nome: 'Fernando'}}></UsuarioInfo>
